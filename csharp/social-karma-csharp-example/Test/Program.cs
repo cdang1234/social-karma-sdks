d89d6ca3645e6d9ac9619937b8e70bd4495b029a
cdang1234 <dang.christopher@gmail.com>
2022-04-26 09:38:37 -0700

    first commit

---
+++ csharp/social-karma-csharp-example/Test/Program.cs
@@ -0,0 +1,15 @@
+﻿using SocialKarma;
+using Newtonsoft.Json.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+      SocialKarma.Client client = new SocialKarma.Client("22f30b08-b169-11ec-b909-0242ac120002");
+      JObject json = client.SubmitReport("123", "20", "title", "description").GetAwaiter().GetResult();
+      Console.WriteLine(json);
+
+      // JObject json = UtilityLibraries.SocialKarma.HelloWorld().GetAwaiter().GetResult();
+      // Console.WriteLine(json);
+    }
+}
---
+++ csharp/social-karma-csharp-sdk/SocialKarma/Class1.cs
@@ -0,0 +1,40 @@
+﻿namespace SocialKarma;
+
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+public class Client
+{
+
+  public Client(string apiKey)
+  {
+    APIKey = apiKey;
+  }
+
+  // Properties.
+  public string APIKey { get; set; }
+  
+  public async Task<JObject> SubmitReport(string reportedUserId, string reportingUserId, string title, string description)
+  {
+    var url = "https://api.socialkarma.xyz/api/v1/report";
+    var client = new HttpClient();
+    client.DefaultRequestHeaders.Add("Auth", this.APIKey);
+
+    var values = new Dictionary<string, string>{
+      { "ReportedUserId", reportedUserId },
+      { "ReportingUserId", reportingUserId },
+      { "Title", title },
+      { "Description", description },
+    };
+
+    var reqJson = JsonConvert.SerializeObject(values, Formatting.Indented);
+    var body = new StringContent(reqJson);
+
+    var response = await client.PostAsync(url, body);
+
+    // extract response body as json and return it
+    string result = response.Content.ReadAsStringAsync().Result;
+    JObject respJson = JObject.Parse(result);
+    return respJson;
+  }
+}
